0e3e0d5ecc9af75ea3988aa2aafae2f0f8954b74
Andrii <sanyoksanyok@i.ua>
2024-11-06 22:34:33 +0100

    Header to Login user ist deleted

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,11 +31,7 @@ const Login = () => {
 
     } else {
 
-      const { data } = await axios.post(backendUrl + '/api/user/login', { email, password }, {headers:  [{ key: "Access-Control-Allow-Credentials", value: "true" },
-                                                                                                                          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                                                                                                                          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                                                                                                                          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-                                                                                                                        ]})
+      const { data } = await axios.post(backendUrl + '/api/user/login', { email, password })
 
       if (data.success) {
         localStorage.setItem('token', data.token)
